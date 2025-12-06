974b70da32bfa4998a4e8ed4d8eeead8240e5145
IrfanRomadhonWidodo <irfan.widodo@mhs.unsoed.ac.id>
2025-12-06 13:00:08 +0700

    screenshot aplikasi

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+    id("com.google.gms.google-services") 
 }
 
 android {
@@ -43,4 +44,4 @@ flutter {
     source = "../.."
 }
 
-apply(plugin = "com.google.gms.google-services")
+
--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -4,8 +4,12 @@ allprojects {
         mavenCentral()
     }
 }
-dependencies {
-    classpath("com.google.gms:google-services:4.4.2")
+
+plugins {
+    id("com.android.application") apply false
+    id("com.android.library") apply false
+    id("org.jetbrains.kotlin.android") apply false
+    id("com.google.gms.google-services") apply false
 }
 
 
